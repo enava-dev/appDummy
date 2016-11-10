de08c1c0bec6378d88ccb4a18d5a7ca2aa639f22
Emmanuel Nava Hernandez <emmanuel.nava@globant.com>
2016-11-09 18:16:04 -0600

    create new structure

--- back/src/server/server.js
+++ back/src/server/server.js
@@ -3,22 +3,44 @@
 var express = require('express'),
     path = require('path'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    routes = require('./routes/routes');
 
 var app = express();
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 
-function test(data, cb){
-  console.log('data:', data);
-  return cb(null, 'success');
-}
+app.use(bodyParser.json());
 
-test('emmanuel', function(err, response){
-  console.log(response);
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 
-test('life code', (err, response) => {
-  console.log('new function');
-  console.log('response:', response);
+
+// Auth Middleware - This will check if the token is valid
+// Only the requests that start with /api/v1/* will be checked for the token.
+// Any URL's that do not follow the below pattern should be avoided unless you 
+// are sure that authentication is not needed
+// app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
+
+
+// Load all the routes
+// app.use('/', routes);
+app.use('/api/v1/', routes);
+// app.use('/api/private/v1');
+
+
+// Start the server
+app.set('port', process.env.PORT || 8080);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
 });
